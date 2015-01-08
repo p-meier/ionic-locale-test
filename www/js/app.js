87d59c8ff9ed805189d6b14fa534d746883aceb7
patmei <patrick.meier@matonu.de>
2015-01-08 14:14:16 +0100

    Initial

---
+++ www/js/app.js
@@ -0,0 +1,55 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'pascalprecht.translate'])
+
+    .config(function ($translateProvider) {
+        $translateProvider.translations('en', {
+            hello_message: "Howdy",
+            goodbye_message: "Goodbye"
+        });
+        $translateProvider.translations('de', {
+            hello_message: "Hallo",
+            goodbye_message: "Tschüss"
+        });
+        $translateProvider.preferredLanguage("en");
+        $translateProvider.fallbackLanguage("en");
+    })
+
+    .run(function ($ionicPlatform, $translate) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+
+            if (navigator.globalization) {
+                navigator.globalization.getPreferredLanguage(function (language) {
+
+                    alert(language.value);
+
+                    $translate.use((language.value).split("-")[0]).then(function (data) {
+                        console.log("SUCCESS -> " + data);
+                    }, function (error) {
+                        console.log("ERROR -> " + error);
+                    });
+
+                }, null);
+            }
+        });
+    })
+
+    .controller("AppCtrl", function ($scope, $translate) {
+
+        $scope.changeLanguage = function (key) {
+            $translate.use(key);
+        };
+
+    })
